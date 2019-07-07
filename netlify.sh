ba58a6ba9f385484fdfc9a960ca113924180c688
Paul Wright <pwright@redhat.com>
2019-07-07 07:58:44 +0100

    update

--- netlify.sh
+++ netlify.sh
@@ -1,20 +1,39 @@
 cd /opt/build/repo
 npm install
+gem install kramdown-asciidoc
 
-export DOC_DIR=docs/books
-export INDEX_DOC=index.adoc
-export NAV_DOC=docs/books/nav.adoc
 
+export DOC_DIR=target/graphback/docs/
+export INDEX_DOC=target/graphback/docs/README.md.adoc
+export NAV_DOC=target/graphback/docs/_sidebar.md.adoc
 mkdir target
 mkdir target/modules
 mkdir target/modules/ROOT
+mkdir target/modules/ROOT/assets
+mkdir target/modules/ROOT/assets/images
+
+
+cd target
+git clone https://github.com/finp/graphback.git
+cd graphback/docs
+find ./ -name "*.md" -type f -exec sh -c \
+    'kramdoc --format=GFM --wrap=ventilate --output={}.adoc {}' \;
+
+
+cd ../../..
+
 cp -Lr $DOC_DIR target/modules/ROOT/pages
+
+rename .md.adoc .adoc target/modules/ROOT/pages/*.md.adoc
+
+
+cp -Lr $DOC_DIR/images target/modules/ROOT/assets/images
 
 cp antora.yml target
 
-cp $NAV_DOC target/modules/ROOT/
+cp $NAV_DOC target/modules/ROOT/nav.adoc
 
-cp -Lr $INDEX_DOC target/modules/ROOT/pages
+cp -Lr $INDEX_DOC target/modules/ROOT/pages/index.adoc
 
 
 node_modules/.bin/antora  ./local-site.yml
--- quick-build.sh
+++ quick-build.sh
@@ -2,6 +2,7 @@ export DOC_DIR=target/graphback/docs/
 export INDEX_DOC=target/graphback/docs/README.md.adoc
 export NAV_DOC=target/graphback/docs/_sidebar.md.adoc
 
+rm -r target
 mkdir target
 mkdir target/modules
 mkdir target/modules/ROOT
@@ -15,9 +16,13 @@ cd graphback/docs
 find ./ -name "*.md" -type f -exec sh -c \
     'kramdoc --format=GFM --wrap=ventilate --output={}.adoc {}' \;
 
+
 cd ../../..
 
 cp -Lr $DOC_DIR target/modules/ROOT/pages
+
+rename .md.adoc .adoc target/modules/ROOT/pages/*.md.adoc
+
 
 cp -Lr $DOC_DIR/images target/modules/ROOT/assets/images
 
@@ -28,4 +33,4 @@ cp $NAV_DOC target/modules/ROOT/nav.adoc
 cp -Lr $INDEX_DOC target/modules/ROOT/pages/index.adoc
 
 
-antora local-site.yml
+antora site.yml
